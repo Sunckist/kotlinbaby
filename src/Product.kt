c55d4a366ebefe7af653c4a2e85debbcc4043107
Magnus Lejonlid <magnus.lejonlid@klarna.com>
2018-04-29 23:07:00 +0200

    Filtering and sorting

--- src/Product.kt
+++ src/Product.kt
@@ -1,16 +1,18 @@
+import java.time.LocalDate
+
 class Product(
         val productId: String,
         val name: String,
         val partNo: String,
-        val createdOn: String,
+        val createdOn: LocalDate,
         val key: String,
         val price: String,
         val currency: String
 ) {
-    var weight: String? = null
-    var material: String? = null
-    var color: String? = null
-    var leadTime: String? = null
+    var weight: String = ""
+    var material: String = ""
+    var color: String = ""
+    var leadTime: String = ""
 
     override fun toString(): String {
         return "Product(productId='$productId', name='$name', partNo='$partNo', createdOn='$createdOn', key='$key', price='$price', currency='$currency', weight='$weight', material='$material', color='$color', leadTime='$leadTime')"
--- src/app.kt
+++ src/app.kt
@@ -1,5 +1,10 @@
 import java.io.File
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
+
+val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
 fun main(args: Array<String>) {
 
@@ -7,33 +12,41 @@ fun main(args: Array<String>) {
     val products = mutableListOf<Product>()
 
     productsInputStream.bufferedReader().useLines { lines ->
-        lines.forEach { line ->
-            val parts = line.split(';')
-            products.add(Product(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6] ))
-        }
+        lines.drop(1) //Skip header row
+                .forEach { line ->
+                    val parts = line.split(';')
+                    products.add(Product(parts[0], parts[1], parts[2], LocalDate.parse(parts[3], dateFormatter), parts[4], parts[5], parts[6]))
+                }
     }
 
     val productsMetaInputStream = File("/Users/magnus.lejonlid/dev/private/kotlinbaby/out/production/kotlinbaby/ProductsMeta.txt").inputStream()
     productsMetaInputStream.bufferedReader().useLines { lines ->
-        lines.forEach { line ->
-            val parts = line.split(";")
-            val match = products.find { p -> p.productId == parts[0] }
-            if (match != null) {
-                match.weight = parts[1]
-                match.material = parts[2]
-                match.color = parts[3]
-                match.leadTime = parts[4]
-            }
-        }
+        lines.drop(1) //Skip header row
+                .forEach { line ->
+                    val parts = line.split(";")
+                    val match = products.find { p -> p.productId == parts[0] }
+                    if (match != null) {
+                        match.weight = parts[1].replace(",", ".")
+                        match.material = parts[2]
+                        match.color = parts[3]
+                        match.leadTime = parts[4]
+                    }
+                }
     }
 
 
 
     products.forEach { p -> println(p) }
 
-    val currencyGroups = products.groupBy { p -> p.currency }
+
+    val currencyGroups = products
+            .filter { p -> p.currency != "USD" && p.material != "Steel" && p.createdOn.dayOfWeek != DayOfWeek.SATURDAY && p.createdOn.dayOfWeek != DayOfWeek.SUNDAY }
+            .groupBy { p -> p.currency }
+
     currencyGroups.forEach { c ->
-        output(c.key, c.value)
+        output(c.key, c.value
+                .sortedBy { p -> p.price }
+        )
     }
 
 }
@@ -45,7 +58,7 @@ fun output(currency: String, products: List<Product>) {
             joiner.add(p.productId)
             joiner.add(p.name)
             joiner.add(p.partNo)
-            joiner.add(p.createdOn)
+            joiner.add(dateFormatter.format(p.createdOn))
             joiner.add(p.price)
             joiner.add(p.weight)
             joiner.add(p.material)
